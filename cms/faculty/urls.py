31aef6939962510a4423909fcb02e501a396b4d5
Nachiket Bhuta <nachiketbhuta@gmail.com>
2019-04-05 00:49:20 +0530

    Added login page and some minor changes in UI

--- cms/faculty/urls.py
+++ cms/faculty/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import create_leave,submit_leave,submit_load_shift
+from .views import create_leave,submit_leave,submit_load_shift, load_login_page
 
 urlpatterns = [
     # path('approve/<int:leave_id>/', update_leave)
+    path('', load_login_page, name="login_page"),
     path('requestleave/',create_leave,name="request_leave"),
   	path('requestleave/submit/', submit_leave, name="submit_leave"),
   	path('requestleave/submit/loadshift/',submit_load_shift,name="loadshifts")
