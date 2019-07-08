89d9800dd4d390e0cf1fb957f2dca72d2a3d4ebb
nachiketbhuta <nachiketbhuta@gmail.com>
2019-07-08 16:57:11 +0530

    Some changes in whitenoise

--- cms/settings.py
+++ cms/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static-root")
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
