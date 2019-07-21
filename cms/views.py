3482dd501b9aa7b2494f75a5a7852b79a3794e04
Harshal Mandlekar <mandlekar.prashant.harshal@gmail.com>
2019-07-21 10:20:58 +0530

    Routued to new home

--- cms/views.py
+++ cms/views.py
@@ -6,6 +6,6 @@ def index(request):
 			if(request.user.is_superuser):
 				return HttpResponseRedirect('/hod/approveleaves')
 			return HttpResponseRedirect('/assistant/updatett')
-		return HttpResponseRedirect('/faculty/requestleave')
+		return HttpResponseRedirect('/faculty')
 	
 	return HttpResponseRedirect('/accounts/login')
