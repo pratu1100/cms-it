df22f836b267b2b01994abfdeef4537b5049dd74
nachiketbhuta <nachiketbhuta@gmail.com>
2019-07-21 17:04:49 +0530

    Connected to RDS

--- cms/settings.py
+++ cms/settings.py
@@ -87,22 +87,22 @@ WSGI_APPLICATION = 'cms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'HOST': 'cmsdbinstance.cfxriz5zuq0g.ap-south-1.rds.amazonaws.com',
-#         'OPTIONS': {
-#             'read_default_file': os.path.join(BASE_DIR, 'db.cnf'),
-#         },
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'cmsdbinstance.cfxriz5zuq0g.ap-south-1.rds.amazonaws.com',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'config', 'db.cnf'),
+        },
+    }
+}
 
 
 
