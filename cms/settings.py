cb0b4ba0685e0c3a48dcc18dd172c93f4b7c5012
nachiketbhuta <nachiketbhuta@gmail.com>
2019-07-23 23:02:26 +0530

    Switched again to SQLite

--- cms/settings.py
+++ cms/settings.py
@@ -16,7 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Change this for environment
-DEVELOPMENT = False
+DEVELOPMENT = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
