1bc5e523b48ae1c67ce7049200bddf7f06d7bcd1
harmanshal <mandlekar.prashant.harshal@gmail.com>
2019-10-22 11:56:19 +0530

    Update development.py
    added azure config in development

--- cms/settings/development.py
+++ cms/settings/development.py
@@ -5,7 +5,10 @@ WSGI_APPLICATION = 'cms.wsgi.development.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'kjsce-cms.database.windows.net',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'config', 'db.cnf'),
+        },
     }
 }
