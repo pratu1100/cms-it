e358a8285de91060cf64f7e81005e31c56dc6c05
Harshal Mandlekar <mandlekar.prashant.harshal@gmail.com>
2019-10-24 23:19:57 +0530

    Django mailer issue resolved

--- cms/settings/development.py
+++ cms/settings/development.py
@@ -5,10 +5,8 @@ WSGI_APPLICATION = 'cms.wsgi.development.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST': 'kjsce-cms.database.windows.net',
-        'OPTIONS': {
-            'read_default_file': os.path.join(BASE_DIR, 'config', 'db.cnf'),
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
